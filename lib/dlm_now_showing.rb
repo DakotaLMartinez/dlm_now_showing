c107b4323575248c9ca5871cdef97e47eb3948d0
Dakota Martinez <dakotaleemusic@gmail.com>
2015-12-23 21:31:42 +0000

    adds GetRequestUrl module to handle generating the proper URL to grab JSON data from gracenote API given a zipcode

--- lib/dlm_now_showing.rb
+++ lib/dlm_now_showing.rb
@@ -1,5 +1,11 @@
-require "dlm_now_showing/version"
+# require "dlm_now_showing/version"
+require 'pry'
 
 module DlmNowShowing
-  # Your code goes here...
+  class GetRequestUrl
+    attr_reader :url
+    def initialize(zip_code)
+       @url = "http://data.tmsapi.com/v1.1/movies/showings?startDate=#{Time.now.strftime("%Y-%m-%d")}&zip=#{zip_code.to_s}&api_key=ju2ketuuttqvsch9jzjstbn8" 
+    end
+  end
 end
