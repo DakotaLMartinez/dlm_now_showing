b4039cd9c60d6b4f4c3d2778e091cf3a0ac2eb7c
Dakota Martinez <dakotaleemusic@gmail.com>
2015-12-29 01:06:42 +0000

    adds basic specs for genres and theatres

---
+++ spec/003_genre_basics_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "Genre Basics" do
+  context Genre do
+    describe "#initializes with a name" do
+      genre = Genre.new("Comedy") 
+      
+      expect(genre.name).to eq("Comedy")
+    end
+    
+    describe ".all" do 
+      it "returns the @@all class variable" do
+        Genre.class_variable_set(:@@all, [])
+        
+        expect(Genre.all).to match_array([])
+      end
+    end
+    
+    describe ".destroy_all" do
+      it "resets the class variable @@all to an empty array" do
+        Genre.class_variable_set(:@@all, ["Movie"])
+        
+        Genre.destroy_all
+        expect(Genre.all).to match_array([])
+      end
+    end
+    
+    describe "#save" do 
+      it "adds movie instances to the @@all class variable" do
+        comedy = Genre.new("Comedy")
+        
+        comedy.save
+        
+        expect(Genre.all).to include(comedy)
+      end
+    end
+    
+    describe ".create" do
+      it "initializes and saves the movie" do
+        comedy = Genre.create("Comedy")
+        
+        expect(Genre.all).to include(comedy)
+      end
+    end
+  end
+end
---
+++ spec/004_theatre_basics_spec.rb
@@ -0,0 +1,52 @@
+require "spec_helper"
+
+describe "Theatre Basics" do
+  context "Theatre" do
+    describe "#intializes with an id" do 
+      theatre = Theatre.new(7792)
+      
+      expect(theater.id).to eq(7792)
+    end
+    
+    describe "#name" do
+      theater = Theatre.new(7792).name = "AMC Loews Marina 6"
+      
+      expect(theater.name).to eq("AMC Loews Marina 6")
+    end
+    
+    describe ".all" do 
+      it "returns the @@all class variable" do
+        Theatre.class_variable_set(:@@all, [])
+        
+        expect(Theatre.all).to match_array([])
+      end
+    end
+    
+    describe ".destroy_all" do
+      it "resets the class variable @@all to an empty array" do
+        Theatre.class_variable_set(:@@all, ["Movie"])
+        
+        Theatre.destroy_all
+        expect(Theatre.all).to match_array([])
+      end
+    end
+    
+    describe "#save" do 
+      it "adds movie instances to the @@all class variable" do
+        theatre = Theatre.new(7792)
+        
+        theatre.save
+        
+        expect(Theatre.all).to include(theatre)
+      end
+    end
+    
+    describe ".create" do
+      it "initializes and saves the movie" do
+        theatre = Theatre.create(7792)
+        
+        expect(Theatre.all).to include(theatre)
+      end
+    end
+  end
+end
