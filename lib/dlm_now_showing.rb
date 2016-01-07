83b12826b3fa157fa17667c0cd8f5212baee60a8
Dakota Martinez <dakotaleemusic@gmail.com>
2016-01-07 03:15:28 +0000

    adds MovieImporter class

--- lib/dlm_now_showing.rb
+++ lib/dlm_now_showing.rb
@@ -8,4 +8,11 @@ module DlmNowShowing
        @url = "http://data.tmsapi.com/v1.1/movies/showings?startDate=#{Time.now.strftime("%Y-%m-%d")}&zip=#{zip_code.to_s}&api_key=#{ENV['API_KEY']}" 
     end
   end
+  
+  class MovieImporter
+    attr_reader :response
+    def initialize(url)
+      @response = HTTParty.get(url).parsed_response
+    end
+  end
 end
